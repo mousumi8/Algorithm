85dfe40c66d4bda5e37c5f629832c60c4359151e
Mousumi Hazarika <hazarika.mousumi.ghy@gmail.com>
2020-05-04 16:32:46 +0530

    Leet Code
    Find the first bad version, use binary search for this

---
+++ VersionControl.java
@@ -0,0 +1,11 @@
+package leetcode.may.challenge;
+
+public class VersionControl {
+
+    boolean isBadVersion(int version){
+        if(version >= 4){
+            return true;
+        }
+        return false;
+    }
+}
